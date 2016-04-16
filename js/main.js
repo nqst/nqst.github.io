7429a84d0885685739efb31ca490d899b39db1c2
Alexander Zaytsev <nqst@MacBook-Air-Alexander.local>
2016-04-16 17:25:29 +0300

    more tweaks!

--- js/main.js
+++ js/main.js
@@ -1,12 +1,1 @@
-$(function(){
-  if($('article').length) {
-    $('article').fitVids({ customSelector: "iframe[src^='http://embed.ted.com']"});
-  };
-  
 
-  // if($('.footer-reveal').length) {
-  //   $('.footer-reveal').footerReveal({
-  //     shadow: false
-  //   });
-  // }
-});
