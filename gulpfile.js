8a58416ca04c311c2915b5643fa702b825e69d53
Alexander Zaytsev <nqst@users.noreply.github.com>
2017-03-07 18:57:59 +0200

    more automatization

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,11 @@ var mqpacker       = require('css-mqpacker');
 var nested         = require('postcss-nested');
 var colorAlpha     = require('postcss-color-alpha');
 var browserSync    = require('browser-sync').create();
-var shell          = require('gulp-shell');
-var cp             = require('child_process');
+var child          = require('child_process');
+var gutil          = require('gulp-util');
+
+var siteRoot = '_site';
+var cssFiles = 'src/css/**/*.css';
 
 var processors = [
   atImport,
@@ -23,7 +26,7 @@ var processors = [
   mqpacker
 ];
 
-gulp.task('css', function(){
+gulp.task('css', function() {
   return gulp.src('src/css/main.css')
     .pipe(postcss(processors))
     .pipe(nano({
@@ -37,56 +40,44 @@ gulp.task('css', function(){
     .pipe(gulp.dest('css/'));
 });
 
-gulp.task('additional-css', function(){
+gulp.task('additional-css', function() {
   return gulp.src(['src/css/posts/**/*'])
     .pipe(gulp.dest('css/posts'));
 });
 
-gulp.task('js', function(){
-  // @todo: add js
+gulp.task('jekyll', function() {
+  var jekyll = child.spawn('jekyll', ['build',
+    '--watch',
+    '--incremental'
+  ]);
+
+  var jekyllLogger = function(buffer) {
+    buffer.toString()
+      .split(/\n/)
+      .forEach((message) => gutil.log('Jekyll: ' + message));
+  };
+
+  jekyll.stdout.on('data', jekyllLogger);
+  jekyll.stderr.on('data', jekyllLogger);
 });
 
-gulp.task('jekyll-build', shell.task(['jekyll build --incremental --watch']));
-
-// gulp.task('jekyll-rebuild', ['jekyll-build'], function(){
-//   browserSync.reload();
-// });
-// gulp.task('jekyll-build', function(done){
-//   return cp.spawn('jekyll', ['build'], {stdio: 'inherit'}).on('close', done);
-// });
-
-gulp.task('watch', function(){
-  gulp.watch('src/css/**/*.css', ['css', 'additional-css']);
-  // gulp.watch('_site/**/*.*').on('change', browserSync.reload);
-  // gulp.watch([
-  //   'index.html',
-  //   'blog/index.html',
-
-  //   '_layouts/*.html',
-  //   '_posts/*.md',
-    
-  //   'css/main.css',
-  //   'js/**/*.js',
-  //   'i/**/*.*'
-  // ], ['jekyll-rebuild']);
-
+gulp.task('watch', function() {
+  gulp.watch(cssFiles, ['css', 'additional-css']);
 });
 
 gulp.task('serve', function() {
 
   browserSync.init({
+    files: [siteRoot + '/**'],
+    port: 4000,
     server: {
-      baseDir: '_site/'
+      baseDir: siteRoot
     },
     open: false,
     notify: false
   });
 
-  
-  //gulp.watch('_site/**/*.*');
-  //gulp.watch('_site/**/*.*').on('change', browserSync.reload);
-  // gulp.watch('_assets/css/main.css').on('change', browserSync.reload);
+  gulp.watch(cssFiles, ['css', 'additional-css']);
 });
 
-// gulp.task('default', ['css', 'jekyll-build', 'watch', 'serve']);
-gulp.task('default', ['css', 'additional-css', 'watch']);
+gulp.task('default', ['css', 'additional-css', 'jekyll', 'serve']);
