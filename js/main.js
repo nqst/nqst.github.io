c3eaf8198798e0ed03b3f0fbf7c7c5d592eeb9a8
Alexander Zaytsev <nqst@users.noreply.github.com>
2017-12-04 23:11:18 +0200

    Sharing!

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+$(function(){
 
+  // Open share modals
+  var $shareLink = $('.js-share-link');
+  $shareLink.click(function (e) {
+    e.preventDefault();
+    var $link = $(this);
+    var href = $link.attr('href');
+    var website = $link.attr('data-website');
+
+    var websites = {
+      facebook: { width: 600, height: 300 },
+      twitter : { width: 600, height: 254 },
+    }
+
+    var modal = function (website) {
+      var options = 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,'
+      window.open(href, '', options + 'height=' + websites[website].height + ',width=' + websites[website].width)
+    }
+
+    modal(website);
+  });
+
+});
